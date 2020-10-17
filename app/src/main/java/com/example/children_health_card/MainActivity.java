bb33b6ed9304ace4a62da5176bf7dfdac25ad3ac
Lucek <julianna.wichowska@gmail.com>
2020-10-17 21:19:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/children_health_card/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.children_health_card;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
